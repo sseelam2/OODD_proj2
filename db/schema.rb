9b30ec9b27a1bfe32e9da598f6be925bda1aec57
Swetha <sseelam2@ncsu.edu>
2018-09-30 19:51:07 -0400

    Validations in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_002110) do
+ActiveRecord::Schema.define(version: 2018_09_30_234735) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "company_id"
