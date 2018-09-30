7c4f3c621a0267245ce5e681231375cbc9fc9ad9
vivin <vivinya25@gmail.com>
2018-09-30 17:34:52 -0400

    Sign in and sign up

--- app/models/signinform.rb
+++ app/models/signinform.rb
@@ -1,3 +1,7 @@
 class Signinform
   attr_accessor :email, :password
+  def initialize(email,password)
+    @email = email
+    @password=password
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'myaccount/index'
   resources :myaccount
-  resources :signup
+ # resources :signup
  # resources :signin
   match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
