2fc5a5a289c0c0685b267e396439110f1b742069
Swetha <sseelam2@ncsu.edu>
2018-09-30 20:05:40 -0400

    Validations in model

---
+++ db/migrate/20180930234735_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :password
+      t.string :email
+
+
+    end
+  end
+end
