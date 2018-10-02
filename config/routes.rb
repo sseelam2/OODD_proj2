00a03cf9357b991333d33b4eae1fb59f269007f5
sseelam2 <swetha.seelam12@gmail.com>
2018-10-02 00:31:39 -0400

    deploy again!

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :myaccount, :househunters
  # resources :signup
  # resources :signin
+ root to "signin#index"
   match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
